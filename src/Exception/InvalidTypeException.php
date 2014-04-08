2afc4f314dba3de2204ffde17a77d4dcb6dfbeac
Sébastien Lavoie <github@lavoie.sl>
2014-04-08 02:18:12 -0400

    added missing doc

--- src/Exception/InvalidTypeException.php
+++ src/Exception/InvalidTypeException.php
@@ -4,6 +4,11 @@ namespace Lavoiesl\AbstractStructure\Exception;
 
 class InvalidTypeException extends StructureException
 {
+    /**
+     * @param string $class
+     * @param string $property
+     * @param string $expected
+     */
     public function __construct($class, $property, $expected)
     {
         parent::__construct("${class}::${property} must be of type '${expected}'");
--- src/Exception/UndefinedPropertyException.php
+++ src/Exception/UndefinedPropertyException.php
@@ -4,6 +4,10 @@ namespace Lavoiesl\AbstractStructure\Exception;
 
 class UndefinedPropertyException extends StructureException
 {
+    /**
+     * @param string $class
+     * @param string $property
+     */
     public function __construct($class, $property)
     {
         parent::__construct("${class}::${property} is undefined");
